95dbf259162ca2ff73ac68abba6b899626ff3884
RivanAzhar <rivanazhar@gmai.com>
2022-12-03 15:58:43 +0700

    Rapihkan folder

--- eduwork-rivan/automate-testing-cypress/cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  chromeWebSecurity: false,
-  pageLoadTimeout: 60000,
-  defaultCommandTimeout: 10000,
-  viewportWidth: 1280,
-  viewportHeight: 720
-});
