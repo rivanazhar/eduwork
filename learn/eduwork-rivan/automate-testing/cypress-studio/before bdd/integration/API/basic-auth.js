b16f2ca4f3fa79d9cb9652f6662c8310c2c35470
RivanAzhar <rivanazhar@gmai.com>
2022-12-18 00:27:13 +0700

    Tugas Basic Authentication API

---
+++ learn/eduwork-rivan/automate-testing/cypress-studio/before bdd/integration/API/basic-auth.js
@@ -0,0 +1,19 @@
+/// <reference types= "cypress" />
+
+describe('Basic authentication', () => {
+    it('Successfully login by appending username and password in URL', function () {
+        cy.visit('https://admin:admin@the-internet.herokuapp.com/basic_auth')
+        cy.get('p').should('include.text', 'Congratulations! You must have the proper credentials.')
+    })
+    
+    it('Successfully login using headers', function () {
+        cy.visit("https://the-internet.herokuapp.com/basic_auth", {
+            headers: {
+                authorization: 'Basic YWRtaW46YWRtaW4='
+            },
+            failOnStatusCode: false
+        })
+        cy.get('p').should('include.text', 'Congratulations! You must have the proper credentials.')
+    })    
+});
+
