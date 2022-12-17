a4a1ef127c6047c1124c11c55d4869d897e6d1a8
RivanAzhar <rivanazhar@gmai.com>
2022-12-17 23:50:10 +0700

    Custom command API Auth

--- learn/eduwork-rivan/automate-testing/cypress-studio/before bdd/support/commands.js
+++ learn/eduwork-rivan/automate-testing/cypress-studio/before bdd/support/commands.js
@@ -24,24 +24,43 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-//custom commands login
-Cypress.Commands.add('login', (username, password) => {
-    cy.clearCookies()
-    cy.clearLocalStorage()
-    cy.get('#user_login').type(username)
-    cy.get('#user_password').type(password)
-    cy.contains('Sign in').click()
-  })
+// //custom commands login
+// Cypress.Commands.add('login', (username, password) => {
+//     cy.clearCookies()
+//     cy.clearLocalStorage()
+//     cy.get('#user_login').type(username)
+//     cy.get('#user_password').type(password)
+//     cy.contains('Sign in').click()
+//   })
 
-//custom commands payment
-Cypress.Commands.add('payment', (payee, account, amount, datepick, description) => {
-    cy.get('#sp_payee').select(payee)
-    cy.get('#sp_account').select(account)
-    cy.get('#sp_amount').type(amount)
-    cy.get('#sp_date').click()
-    cy.contains('21').click()
-    cy.get('#sp_description').type(description)
-    cy.get('#pay_saved_payees').click()
+// //custom commands payment
+// Cypress.Commands.add('payment', (payee, account, amount, datepick, description) => {
+//     cy.get('#sp_payee').select(payee)
+//     cy.get('#sp_account').select(account)
+//     cy.get('#sp_amount').type(amount)
+//     cy.get('#sp_date').click()
+//     cy.contains('21').click()
+//     cy.get('#sp_description').type(description)
+//     cy.get('#pay_saved_payees').click()
+// })
+  
+//custom comman api authentication
+
+Cypress.Commands.add('loginViaAPI', (
+  username = Cypress.env('userName'),
+  password = Cypress.env('userPassword')
+) => {
+  cy.request('POST', `${Cypress.env('apiUrl')}users/login/auth`, {
+    username: username,
+    password: password
+    })
+    .then((response) => {
+      cy.setCookie('sessionId', response.body.sessionId)
+      cy.setCookie('userId', response.body.userId)
+      cy.setCookie('userName', response.body.userName)
+      expect(response.body.sessionId).equal('basicauth')
+      expect(response.body.userId).equal('21')
+      expect(response.body.userName).equal('admin')
+      expect(response.body.message).equal('Authentication succes, welcome')
+  })
 })
-  
-  
